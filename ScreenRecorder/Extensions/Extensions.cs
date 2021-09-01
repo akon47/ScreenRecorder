c53452d67149f409140a1eea9d430391cd9d99ad
akon47 <akon47@naver.com>
2021-09-01 11:24:46 +0900

    feat: 팝업 윈도우의 핸들을 얻는 확장 메서드 추가 리플렉션을 이용하여 private 클래스와 메서드를 참조하여 구현. 나중에 WPF 코드가 변경되면 동작에 문제가 생김.

---
+++ ScreenRecorder/Extensions/Extensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScreenRecorder.Extensions
+{
+    public static class PopupExtension
+    {
+        public static IntPtr GetPopupWindowHandle(this System.Windows.Controls.Primitives.Popup popup)
+        {
+            /// There is no way to get the handle of the pop-up window in the usual way, so it is obtained by reflection.
+            /// Popup.cs code may not work if changed!!!
+            /// Last commit hash for code referenced: 58e7355
+            /// https://github.com/dotnet/wpf/blob/main/src/Microsoft.DotNet.Wpf/src/PresentationFramework/System/Windows/Controls/Primitives/Popup.cs
+            try
+            {
+                var _secHelper = popup.GetType().GetField("_secHelper", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)?.GetValue(popup);
+                if (_secHelper?.GetType().GetProperty("Handle", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)?.GetValue(_secHelper) is IntPtr popupHandle)
+                {
+                    return popupHandle;
+                }
+                else
+                {
+                    return IntPtr.Zero;
+                }
+            }
+            catch
+            {
+                return IntPtr.Zero;
+            }
+        }
+    }
+}
